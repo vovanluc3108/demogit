a99ab65e1f4d25bbe7f479c9754f70b89af6983c
PCI0018 <pci0018@PCI0018.local>
2019-06-12 11:32:34 +0700

    new learn jva

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 {\rtf1\ansi\ansicpg1252\cocoartf1671
-{\fonttbl\f0\fnil\fcharset0 Menlo-Bold;\f1\fnil\fcharset0 Menlo-Regular;}
+{\fonttbl\f0\fswiss\fcharset0 Arial-BoldMT;\f1\fswiss\fcharset0 ArialMT;}
 {\colortbl;\red255\green255\blue255;\red80\green96\blue184;\red107\green0\blue109;\red236\green236\blue236;
 \red115\green0\blue2;\red117\green117\blue117;}
 {\*\expandedcolortbl;;\cssrgb\c38824\c46667\c77255;\cssrgb\c50196\c0\c50196;\cssrgb\c94118\c94118\c94118;
@@ -28,5 +28,4 @@
 \f0\b \cf2 \cb1 \strokec2 out
 \f1\b0 \cf3 \cb4 \strokec3 .println(\cf5 \cb1 \strokec5 "Hello World"\cf3 \cb4 \strokec3 );\
     \}\
-\
 \}}
